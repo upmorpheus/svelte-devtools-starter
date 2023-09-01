19a425a1481aa3e3fcbdf90ee2648e86d9e756d8
Morpheus <mangoo.pay@gmail.com>
2023-09-01 01:55:02 -0400

    complete
    svelte app complete

---
+++ test/src/Events/EventsComponent.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+	const handler = () => dispatch('click', 'I was clicked!');
+</script>
+
+<button type="button" on:click|once|stopPropagation|preventDefault={handler}> Click me! </button>
